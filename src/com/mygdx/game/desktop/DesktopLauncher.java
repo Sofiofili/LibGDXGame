9acd19f2d7cb4d5d859343399adcac9d55d6858c
Eermou <Eermou@LAPTOP-AO0UJUBB>
2024-02-19 18:37:49 +0100

    commit

---
+++ src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import view.TankGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 20*40;
+		config.width = 20*40;
+		TankGame game = new TankGame();
+		new LwjglApplication(game, config);
+	}
+}
